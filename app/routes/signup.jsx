5243aa1641c918539db8977efc58ec87901fbcb4
tinaemmik <98392184+tinaemmik@users.noreply.github.com>
2024-03-07 13:20:49 +0100

    events page add

--- app/routes/signup.jsx
+++ app/routes/signup.jsx
@@ -1,11 +1,51 @@
 import {Form, Link} from "@remix-run/react";
 
+//what is in meta data
+export function meta() {    
+    return [
+        {
+            title: "Sign Up",
+            description: "Sign up for an account",
+        }
+    ]
+};
+
+// LOADER FUNCTION
+
+
+//ACTION FUNCTION
+
+
 export default function SignUp() {
     return(
-        <div className="flex h-screen justify-center items-center">
-            <div className="w-96">
-                <h1 className="text-3xl font-bold mb-4">Sign Up</h1>
-                <Form method="post" className="space-y-4">
+        <div className="flex min-h-full flex-1 flex-col mt-20 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h1 className="mt-6 text-center text-2xl font-bold tracking-tight text-gray-900">Sign Up</h1>
+                <Form method="post" className="space-y-6 ">
+                    <div>
+                        <label htmlFor="name" className="block text-sm font-medium">
+                            Name
+                        </label>
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            required
+                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                        />
+                    </div>
+                    <div>
+                    <label htmlFor="city" className="block text-sm font-medium">
+                            City
+                        </label>
+                        <input
+                            id="city"
+                            name="city"
+                            type="text"
+                            required
+                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                        />
+                    </div>
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium">
                             Email
