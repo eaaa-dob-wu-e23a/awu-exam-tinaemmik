2aac13cf9e58210124de701e36068a6527f38e32
tinaemmik <98392184+tinaemmik@users.noreply.github.com>
2024-03-08 19:14:43 +0100

    styling added

--- app/routes/signin.jsx
+++ app/routes/signin.jsx
@@ -8,13 +8,12 @@ export default function SignIn() {
                 <div className="flex w-8/12  bg-gray-200 rounded-xl mx-auto shadow-lg overflow-hidden">
                     <div className="w-1/2 py-10 px-8">
                         <h1 className="text-3xl px-10 text-gray-700 font-bold mb-4">Velkommen til Teater events!</h1>
-                        <img src="../public/image/logo.png" alt="Teater" className="w-3/4 mx-auto rounded-md"/>
                         <p className="px-10 text-gray-600">Her kan du finde og tilføje events</p>
 
                     </div>
                     <div className="w-1/2 py-10 px-8">
                     <h2 className="text-3xl text-gray-700 font-bold mb-4">Login</h2>
-                    <Form method="post" className="space-y-4">
+                    <Form method="post" className="space-y-6">
                         <div>
                             <label htmlFor="email" className="block text-md font-medium text-gray-600">
                                 Email
@@ -51,7 +50,7 @@ export default function SignIn() {
                     <div className="mt-4">
                         <p className="text-gray-500">Don't have an account?</p>
                         <Link to="/signup" className="text-blue-600">
-                            <u>Create an account</u>
+                            <u>Create account</u>
                         </Link>
                     </div>
                     </div>
--- app/routes/signup.jsx
+++ app/routes/signup.jsx
@@ -18,12 +18,18 @@ export function meta() {
 
 export default function SignUp() {
     return(
-        <div className="flex min-h-full flex-1 flex-col mt-20 sm:px-6 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <h1 className="mt-6 text-center text-2xl font-bold tracking-tight text-gray-900">Sign Up</h1>
-                <Form method="post" className="space-y-6 ">
+        <div className="min h-screen py-40 bg-gray-700">
+            <div className="container mx-auto">
+            <div className="flex w-8/12  bg-gray-200 rounded-xl mx-auto shadow-lg overflow-hidden">
+                <div className="w-1/2 py-10 px-8">
+                    <h1 className="text-3xl px-10 text-gray-700 font-bold mb-4">Welcome to Teater events!</h1>
+                    <p className="px-10 text-gray-600">Here you can find and add events</p>
+                </div>
+                <div className="w-1/2 py-10 px-8">
+                <h1 className="text-3xl text-gray-700 font-bold mb-4">Create account</h1>
+                <Form method="post" className="space-y-6">
                     <div>
-                        <label htmlFor="name" className="block text-sm font-medium">
+                        <label htmlFor="name" className="block text-md font-medium text-gray-600">
                             Name
                         </label>
                         <input
@@ -31,11 +37,11 @@ export default function SignUp() {
                             name="name"
                             type="text"
                             required
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                            className="mt-1 block w-2/3 h-8 rounded-sm border-gray-400 shadow-md"
                         />
                     </div>
                     <div>
-                    <label htmlFor="city" className="block text-sm font-medium">
+                    <label htmlFor="city" className="block text-md font-medium text-gray-600">
                             City
                         </label>
                         <input
@@ -43,11 +49,11 @@ export default function SignUp() {
                             name="city"
                             type="text"
                             required
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                            className="mt-1 block w-2/3 h-8 rounded-sm border-gray-400 shadow-md"
                         />
                     </div>
                     <div>
-                        <label htmlFor="email" className="block text-sm font-medium">
+                        <label htmlFor="email" className="block text-md font-medium text-gray-600">
                             Email
                         </label>
                         <input
@@ -55,11 +61,11 @@ export default function SignUp() {
                             name="email"
                             type="email"
                             required
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                            className="mt-1 block w-2/3 h-8 rounded-sm border-gray-400 shadow-md"
                         />
                     </div>
                     <div>
-                        <label htmlFor="password" className="block text-sm font-medium">
+                        <label htmlFor="password" className="block text-md font-medium text-gray-600">
                             Password
                         </label>
                         <input
@@ -67,22 +73,25 @@ export default function SignUp() {
                             name="password"
                             type="password"
                             required
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+                            className="mt-1 block w-2/3 h-8 rounded-sm border-gray-400 shadow-md"
                         />
                     </div>
                     <div>
                         <button
                             type="submit"
-                            className="w-full bg-indigo-600 text-white py-2 rounded-md"
+                            className="w-2/3 bg-blue-500 text-white py-2 rounded-md"
                         >
-                            Sign Up
+                            Create account
                         </button>
                     </div>
                 </Form>
                 <div className="mt-4">
-                    <Link to="/signin" className="text-indigo-600">
-                        Already have an account? Sign In
+                    <p className="text-gray-500">Already have an account?</p>
+                    <Link to="/signin" className="text-blue-600">
+                        <u>Sign in</u>
                     </Link>
+                </div>
+                </div>
                 </div>
             </div>
         </div>
