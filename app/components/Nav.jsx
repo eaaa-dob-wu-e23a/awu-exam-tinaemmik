eeb0ac1ae6622ccb624715ea012a2542b493708a
tinaemmik <98392184+tinaemmik@users.noreply.github.com>
2024-03-11 12:31:50 +0100

    Nav edited and session.server add

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,9 +5,8 @@ export default function Nav() {
         <div className="flex w-full h-20 bg-gray-700 px-10 py-7 font-semibold font-sans">
             <h2 className="text-2xl text-gray-200">Teater events</h2>
         <nav className="text-lg text-white items-center justify-center px-40">
-            <NavLink to="/dashboard" className="px-10 hover:text-blue-400">Dashboard</NavLink>
+        <NavLink to="/events"className="px-10 hover:text-blue-400">Events</NavLink>
             <NavLink to="/profil"className="px-10 hover:text-blue-400">Profil</NavLink>
-            <NavLink to="/events"className="px-10 hover:text-blue-400">Events</NavLink>
             <NavLink to="#"className="px-10 hover:text-blue-400">Tilføj Event</NavLink>
         </nav>
         </div>
