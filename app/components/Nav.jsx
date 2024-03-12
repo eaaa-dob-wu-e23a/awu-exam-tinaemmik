72b9d8b22dac93e33ce15cb781d42f8f8ab7244f
tinaemmik <98392184+tinaemmik@users.noreply.github.com>
2024-03-12 16:34:49 +0100

    indsat navigation til diverse sider igen

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,9 +5,9 @@ export default function Nav() {
         <div className="flex w-full h-20 bg-gray-700 px-10 py-7 font-semibold font-sans">
             <h2 className="text-2xl text-gray-200">Teater events</h2>
         <nav className="text-lg text-white items-center justify-center px-40">
-            <NavLink to="#"className="px-10 hover:text-blue-400">Events</NavLink>
-            <NavLink to="#"className="px-10 hover:text-blue-400">Profil</NavLink>
-            <NavLink to="#"className="px-10 hover:text-blue-400">Tilføj Event</NavLink>
+            <NavLink to="/events"className="px-10 hover:text-blue-400">Events</NavLink>
+            <NavLink to="/profile"className="px-10 hover:text-blue-400">Profil</NavLink>
+            <NavLink to="/add-event"className="px-10 hover:text-blue-400">Tilføj Event</NavLink>
         </nav>
         </div>
     )
